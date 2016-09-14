8f53d9402bea05f264a78a67e7d0043799b38bb8
Frank Peelen <frank.peelen@yahoo.com>
2016-09-14 13:52:59 +0800

    Readied the CIFAR-10 data for use in Deep Learning

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
-
+import pandas
+import numpy as np
+import cPickle as pickle
 
 def main():
-	# Take the CIFAR-10 Data, and create an algorithm that can label images from the test set with as low as possible error.
-
-
 	# Download, load, and prepare the CIFAR-10 data.
 
 	# Download the CIFAR-10 data.
-	# DONE
+	# DONE. Located in /home/peel/Downloads/cifar10py/
 
-	## Load the CIFAR-10 data into C++.
+	## Load the CIFAR-10 data into Python.
 
-	"""
-	For each file:
-	Input = 30730000 bytes
-	Desired output = y(10.000,1) & x(10.000,3072)
-	"""
+	# Unpickle the test data, and put them into x_test & y_test
+	unpickled_test_data = unpickle('/home/peel/Downloads/cifar10py/test_batch')
+	x_test = unpickled_test_data['data']
+	y_test = unpickled_test_data['labels']
+	y_test = np.asarray(y_test)
+
+	# Unpickle all 5 data files to be used for training.
+	unpickled_data_1 = unpickle('/home/peel/Downloads/cifar10py/data_batch_1')
+	unpickled_data_2 = unpickle('/home/peel/Downloads/cifar10py/data_batch_2')
+	unpickled_data_3 = unpickle('/home/peel/Downloads/cifar10py/data_batch_3')
+	unpickled_data_4 = unpickle('/home/peel/Downloads/cifar10py/data_batch_4')
+	unpickled_data_5 = unpickle('/home/peel/Downloads/cifar10py/data_batch_5')
+	# Put the unpickled data into x_train & y_train
+	x_train = np.append(unpickled_data_1['data'], unpickled_data_2['data'], axis=0)
+	x_train = np.append(x_train, unpickled_data_3['data'], axis=0)
+	x_train = np.append(x_train, unpickled_data_4['data'], axis=0)
+	x_train = np.append(x_train, unpickled_data_5['data'], axis=0)
+	y_train = np.append(unpickled_data_1['labels'], unpickled_data_2['labels'])
+	y_train = np.append(y_train, unpickled_data_3['labels'])
+	y_train = np.append(y_train, unpickled_data_4['labels'])
+	y_train = np.append(y_train, unpickled_data_5['labels'])
+
+	# Optional test to check if everything went correctly.
+	# print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 
 	# Fix and correct missing data.
+	# print(x_train.min(), x_train.max())
+	# print(y_train.min(), y_train.max())
+	# print(x_test.min(), x_test.max())
+	# print(y_test.min(), y_test.max())
+	# The data looks fine. No crazy outliers. All values are inbetween 0 and 255.
 
-	#  Preprocess data --> Normalize + zero-mean
-
-	"""
-	Fit the data neatly into:
-	x_train 	& 	y_train
-	x_val 	& 	y_val
-	x_test 	& 	y_test
-	sets for training, validation, and testing.
-	"""
-
+	# Preprocess the data --> Make the features have zero-mean.
+	mean = x_train.mean(axis=0)
+	x_train = x_train - mean
+	x_test = x_test - mean
 
 	# Build a neural network to serve as the algorithm.
 
@@ -39,3 +56,13 @@ def main():
 	# Tweak & tune the NN and it's hyperparameters on a validation set.
 
 	# Test it's performance on the test set for a final result.
+
+# Unpickle a file.
+# Returns a dictionairy with the file's contents.
+def unpickle(file):
+    fo = open(file, 'rb')
+    dict = pickle.load(fo)
+    fo.close()
+    return dict
+
+main()
